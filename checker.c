66344b68e7d52a2c6dbeb1e64f2a60243df8b208
deepaksubramany <deepakpearl0301@gmail.com>
2024-08-01 09:39:46 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -6,30 +6,28 @@ int batteryIsOk(float temperature, float soc, float chargeRate)
 {
     int isOk = 1;
 
-    // Consolidate temperature checks
-    if (temperature < 0 || temperature > 45) {
+    // Combine all conditions into one check using logical OR
+    if (temperature < 0 || temperature > 45 || soc < 20 || soc > 80 || chargeRate > 0.8) {
         if (temperature < 0) {
             printf("Temperature too low!\n");
-        } else {
+            isOk = 0;
+        } else if (temperature > 45) {
             printf("Temperature too high!\n");
+            isOk = 0;
         }
-        isOk = 0;
-    }
-
-    // Consolidate state of charge (SOC) checks
-    if (soc < 20 || soc > 80) {
+        
         if (soc < 20) {
             printf("State of Charge too low!\n");
-        } else {
+            isOk = 0;
+        } else if (soc > 80) {
             printf("State of Charge too high!\n");
+            isOk = 0;
         }
-        isOk = 0;
-    }
-
-    // Consolidate charge rate check
-    if (chargeRate > 0.8) {
-        printf("Charge Rate too high!\n");
-        isOk = 0;
+        
+        if (chargeRate > 0.8) {
+            printf("Charge Rate too high!\n");
+            isOk = 0;
+        }
     }
 
     return isOk;
@@ -37,7 +35,6 @@ int batteryIsOk(float temperature, float soc, float chargeRate)
 
 int main()
 {
-    // Tests
     assert(batteryIsOk(25.0, 50.0, 0.5)); 
     assert(!batteryIsOk(50.0, 70.0, 0.6)); 
     assert(!batteryIsOk(-5.0, 70.0, 0.6)); 
