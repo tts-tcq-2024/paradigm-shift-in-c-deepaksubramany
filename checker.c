f0f2d21310f08bf4706916bdec194f51a24d5d4d
deepaksubramany <deepakpearl0301@gmail.com>
2024-08-01 09:24:00 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <assert.h>
 
-// Function to check battery status and print error messages
 int batteryIsOk(float temperature, float soc, float chargeRate)
 {
     int isOk = 1;
     
-    // Check temperature
     if (temperature < 0) {
         printf("Temperature too low!\n");
         isOk = 0;
@@ -14,8 +12,7 @@ int batteryIsOk(float temperature, float soc, float chargeRate)
         printf("Temperature too high!\n");
         isOk = 0;
     }
-
-    // Check SOC (State of Charge)
+ 
     if (soc < 20) {
         printf("State of Charge too low!\n");
         isOk = 0;
@@ -24,7 +21,7 @@ int batteryIsOk(float temperature, float soc, float chargeRate)
         isOk = 0;
     }
 
-    // Check charge rate
+  
     if (chargeRate > 0.8) {
         printf("Charge Rate too high!\n");
         isOk = 0;
@@ -35,14 +32,12 @@ int batteryIsOk(float temperature, float soc, float chargeRate)
 
 int main()
 {
-    // Test cases
-    assert(batteryIsOk(25.0, 50.0, 0.5)); // Should pass, all values are okay
+    assert(batteryIsOk(25.0, 50.0, 0.5)); 
 
-    assert(!batteryIsOk(50.0, 70.0, 0.6)); // Should fail, Temperature too high
-    assert(!batteryIsOk(-5.0, 70.0, 0.6)); // Should fail, Temperature too low
-    assert(!batteryIsOk(25.0, 15.0, 0.6)); // Should fail, SOC too low
-    assert(!batteryIsOk(25.0, 85.0, 0.6)); // Should fail, SOC too high
-    assert(!batteryIsOk(25.0, 70.0, 0.9)); // Should fail, Charge Rate too high
-    
+    assert(!batteryIsOk(50.0, 70.0, 0.6)); 
+    assert(!batteryIsOk(-5.0, 70.0, 0.6)); 
+    assert(!batteryIsOk(25.0, 15.0, 0.6)); 
+    assert(!batteryIsOk(25.0, 85.0, 0.6)); 
+    assert(!batteryIsOk(25.0, 70.0, 0.9)); 
     return 0;
 }
